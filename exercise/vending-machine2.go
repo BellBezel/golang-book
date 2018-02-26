a768d9b5a37f085648aaf4063a270e009d73ca94
Bell <nattama.ji@tnis.com>
2018-02-26 10:30:12 +0700

    Change input to be 10 in vending-machine2.go

--- exercise/vending-machine2.go
+++ exercise/vending-machine2.go
@@ -6,11 +6,17 @@ type VendingMachine struct {
 }
 
 func (m VendingMachine)  InsertedMoney() int {
-	return 0
+	return 10
+}
+
+func (vm *VendingMachine)  InsertCoin(coin string){
 }
 
 func main() {
 	vm := VendingMachine{}
 	fmt.Println("Inserted Money:", vm.InsertedMoney())
 	// Inserted Money: 0
+	vm.InsertCoin("T")
+	fmt.Println("Inserted Money:", vm.InsertedMoney())
+	// Inserted Money: 10
 }
