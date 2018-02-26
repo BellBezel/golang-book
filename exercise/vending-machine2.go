d82768e8bdcfbe537772484a71e034f17664b88b
Bell <nattama.ji@tnis.com>
2018-02-26 10:27:27 +0700

    Add vending-machine2.go

---
+++ exercise/vending-machine2.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+type VendingMachine struct {
+}
+
+func (m VendingMachine)  InsertedMoney() int {
+	return 0
+}
+
+func main() {
+	vm := VendingMachine{}
+	fmt.Println("Inserted Money:", vm.InsertedMoney())
+	// Inserted Money: 0
+}
