4a66e3c6b3631808bfc503485e4166f3986c7915
Bell <nattama.ji@tnis.com>
2018-03-07 14:09:24 +0700

    Add chapter13-15

---
+++ chapter13-15/main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	ch := make(chan int, 10)
+	go fibonacci(cap(ch), ch)
+
+	for i := range ch {
+		fmt.Println(i)
+	}
+}
+
+func fibonacci(n int, ch chan int) {
+	x, y := 0, 1
+	for i := 0; i < n; i++ {
+		ch <- x
+		x, y = y, x+y
+	}
+	close(ch)     //ถ้า comment ตัว close ไป คำสั่ง range ใน main จะไม่รู้ว่า channal นี้จบแล้ว และจะรอไปเรื่อยๆ ทำให้เกิด Deadlock
+}
+
+/* OUTPUT :
+0
+1
+1
+2
+3
+5
+8
+13
+21
+34
+*/
