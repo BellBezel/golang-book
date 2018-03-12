9649e3da2200810894313517313f5ae09fc2807b
Bell <nattama.ji@tnis.com>
2018-03-12 10:19:16 +0700

    Add function to chapter14-1

--- chapter14-1/main.go
+++ chapter14-1/main.go
@@ -11,9 +11,17 @@ func main() {
 		func(w http.ResponseWriter, r *http.Request) {
 			fmt.Fprintln(w, "Hello, World!")
 		})
+	barHandler := func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintln(w, "Hello Bar!")
+	}
+	http.HandleFunc("/bar", barHandler) //ถ้าใส่ /bar ที่ url จะ print "Hello Bar!"
+
 	http.ListenAndServe(":3000", nil) //ตรง nil เป็นจุดที่สามารถใส่สิ่งที่ต้องการค้นหาเพิ่มเติมได้ แต่ถ้าใส่ nil หมายถึงไม่ต้องการค้นหา
 }
 
 /* เข้า Web Browser: http://localhost:3000/
 OUTPUT : Hello, World!
+
+เข้า Web Browser: http://localhost:3000/bar
+OUTPUT : Hello Bar!
 */
