85f78dff8d3b25247f1edcf5e1417dc4915cce72
Bell <nattama.ji@tnis.com>
2018-03-12 10:15:03 +0700

    Addcomment to chapter14-1

--- chapter14-1/main.go
+++ chapter14-1/main.go
@@ -7,11 +7,11 @@ import (
 
 func main() {
 	http.HandleFunc(
-		"/",
+		"/", //บอกจุดเริ่มต้น ว่าจะเริ่มดูตรงนี้
 		func(w http.ResponseWriter, r *http.Request) {
 			fmt.Fprintln(w, "Hello, World!")
 		})
-	http.ListenAndServe(":3000", nil)
+	http.ListenAndServe(":3000", nil) //ตรง nil เป็นจุดที่สามารถใส่สิ่งที่ต้องการค้นหาเพิ่มเติมได้ แต่ถ้าใส่ nil หมายถึงไม่ต้องการค้นหา
 }
 
 /* เข้า Web Browser: http://localhost:3000/
