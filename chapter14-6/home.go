cfadc833e5623f20cc551f8f1156e7f79fc91006
Bell <nattama.ji@tnis.com>
2018-03-14 10:15:55 +0700

    Modify home.go to use mux

--- chapter14-6/home.go
+++ chapter14-6/home.go
@@ -1,13 +1,16 @@
 package main
 
 import (
+	"github.com/gorilla/mux"
 	"net/http"
 	"fmt"
 )
 
 func HomePageHandler(w http.ResponseWriter, r *http.Request) {
-	name := r.URL.Query().Get("name")
-	if name == "" {
+	vars := mux.Vars(r)
+	name := vars["name"]
+
+	if vars["name"] == "" {
 		name = "World"
 	}
 	fmt.Fprintf(w, "Hello, %s!", name)
@@ -17,9 +20,13 @@ func UsersHandle(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Users Page")
 }
 
+func NewRouter() http.Handler {
+	r := mux.NewRouter()
+	r.HandleFunc("/{name}", HomePageHandler).Methods("GET")
+	r.HandleFunc("/users", UsersHandle).Methods("GET")
+	return r
+}
 
 func main() {
-	http.HandleFunc("/", HomePageHandler)
-	http.HandleFunc("/users", UsersHandle)
-	http.ListenAndServe(":3000", nil) 
+	http.ListenAndServe(":3000", NewRouter()) 
 }
