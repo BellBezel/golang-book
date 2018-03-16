0fc8c0f4e8f8fb4a67615d6746314cb076f60074
Bell <nattama.ji@tnis.com>
2018-03-16 10:13:11 +0700

    Add city condition

--- weather-web/home.go
+++ weather-web/home.go
@@ -15,8 +15,30 @@ func HomePageHandler(w http.ResponseWriter, r *http.Request) {
 		name = "World"
 	}
 	fmt.Fprintf(w, "Hello, %s!", name)*/
-	fmt.Fprintln(w, "Hobart")
-	fmt.Fprintln(w, "14c shower rain")
+	vars := mux.Vars(r)
+	name := vars["name"]
+	if name == "hobart" {
+		fmt.Fprintln(w, "Hobart")
+		fmt.Fprintln(w, "14c shower rain")
+	}
+
+	if name == "all" {
+		fmt.Fprintln(w, "Hobart")
+		fmt.Fprintln(w, "14c shower rain\n")
+
+		fmt.Fprintln(w, "New York")
+		fmt.Fprintln(w, "0c broken clouds\n")
+
+		fmt.Fprintln(w, "Kupang")
+		fmt.Fprintln(w, "20c clear sky\n")
+
+		fmt.Fprintln(w, "Nairobi")
+		fmt.Fprintln(w, "16c moderate rain\n")
+
+		fmt.Fprintln(w, "Bangkok")
+		fmt.Fprintln(w, "33c few clouds\n")
+	}
+
 }
 
 func UsersHandle(w http.ResponseWriter, r *http.Request) {
@@ -25,7 +47,7 @@ func UsersHandle(w http.ResponseWriter, r *http.Request) {
 
 func NewRouter() http.Handler {
 	r := mux.NewRouter()
-	r.HandleFunc("/weather/hobart", HomePageHandler).Methods("GET")
+	r.HandleFunc("/weather/{name}", HomePageHandler).Methods("GET")
 	r.HandleFunc("/users", UsersHandle).Methods("GET")
 	return r
 }
